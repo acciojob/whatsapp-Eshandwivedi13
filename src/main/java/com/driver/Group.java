831601185fda6c3b3bf91159b173775d937e5f43
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-01-30 20:16:38 +0530

    First

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -4,4 +4,24 @@ public class Group {
     private String name;
     private int numberOfParticipants;
 
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
+
+    public Group(String name, int numberOfParticipants) {
+        this.name = name;
+        this.numberOfParticipants = numberOfParticipants;
+    }
 }
